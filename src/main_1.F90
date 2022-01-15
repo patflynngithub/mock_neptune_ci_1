c6710171f6b7915c72a15e8a8b0b4b6bb5507874
Patrick Flynn <patrick.flynn.ctr@nrlmry.navy.mil>
2022-01-14 16:30:36 -0800

    add fortran timing simulation code

--- src/main_1.F90
+++ src/main_1.F90
@@ -17,6 +17,20 @@ use a_module
 
 implicit none
 
+integer :: num_cmdline_args
+
+num_cmdline_args = command_argument_count()
+
+if (num_cmdline_args /= 3) then
+   write(*,*) "# of command line arguments: ", num_cmdline_args
+   write(*,*) "Error: exactly two command line parameters are required"
+   write(*,*) "       - fail/succeed            (first parameter)"
+   write(*,*) "       - output_matrix_file_*    (second parameter)"
+   write(*,*) "             * = none, accurate, inaccurate or error"
+   write(*,*) "       - good_timing/bad_timing  (third parameter)"
+   stop 2
+end if
+
 ! is an execution success or failure desired?
 call success_or_failure()
 
@@ -24,6 +38,12 @@ call success_or_failure()
 
 ! Is an output matrix file desired? If so, accurate or inaccurate (Quality Assurance)?
 call output_matrix_file()
+
+!-------------------------
+
+! Is a good (small) or bad (too large) timing desired
+! to be output to nep.error.000000? 
+call good_or_bad_timing()
 
 end program
 
